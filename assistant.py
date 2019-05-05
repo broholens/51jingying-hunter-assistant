722bd09c8ccec03ffbdae60eb8429cf42dc1a489
broholens <broholens@outlook.com>
2019-05-05 15:35:47 +0800

    解决投递失败导致投递数达不到20的问题

--- assistant.py
+++ assistant.py
@@ -47,8 +47,7 @@ class HunterAssistant:
     def recommend(self, manager_id, case_id):
         # 递名片
         data = {'userid': manager_id+'_1', 'caseid': case_id}
-        resp = request(self.post_url, method='post', json=data, cookies=self.cookie)
-        print(resp.content.decode('gb2312'))
+        resp = request(self.post_url, method='post', data=data, cookies=self.cookie)
         if not resp:
             return False
         return True if resp.json().get('msg') == '递送成功' else False
@@ -57,17 +56,7 @@ class HunterAssistant:
         print('*'*40)
         print('当前猎头为', self.username)
         print('*'*40)
-        professional_score, delivered_count = self.get_basic_info()
-        if professional_score == -1:
-            print('未获取到专业值！ 请尝试更新cookie！')
-            return 
-        if professional_score < 200:
-            # BUG: 重新启动程序，专业值小于200且已经投递过简历
-            delivered_count = 0
-        print('目前专业值为{}, 今日已递出{}个名片'.format(professional_score, delivered_count))
-        # 每天投递20个
-        remaining = 20 - delivered_count
-        # 获取第一页经理人id
+        # 获取经理人id
         managers = self.get_managers_ids()
         if managers == []:
             print('未获取到经理人ID！ 请尝试更新cookie！ ')
@@ -75,6 +64,25 @@ class HunterAssistant:
         print('获取经理人信息成功！')
         # 打乱序列，解决下面pop的问题
         random.shuffle(managers)
+        # while循环解决投递失败导致投递数达不到20的问题
+        while 1:
+            professional_score, delivered_count = self.get_basic_info()
+            if professional_score == -1:
+                print('未获取到专业值！ 请尝试更新cookie！')
+                return 
+            if professional_score < 200:
+                # BUG: 重新启动程序，专业值小于200且已经投递过简历
+                delivered_count = 0
+            print('目前专业值为{}, 今日已递出{}个名片'.format(professional_score, delivered_count))
+            if delivered_count >= 20:
+                break
+            self._deliever_card(managers, delivered_count)
+        # 打印投递完成信息
+        print(self.username, '今日投递任务完成！')
+
+    def _deliever_card(self, managers, delivered_count):
+        # 每天投递20个
+        remaining = 20 - delivered_count
         # 投递
         while remaining > 0 and len(managers) > 0:
             manager = managers.pop()
@@ -84,14 +92,9 @@ class HunterAssistant:
             else:
                 print('递送失败！经理人id:', manager)
             time.sleep(random.random()*30)
-        # 打印投递完成信息
-        professional_score, delivered_count = self.get_basic_info()
-        print(self.username, '今日投递任务完成！')
-        print('目前专业值为{}, 今日已递出{}个名片'.format(professional_score, delivered_count))
-
 
 if __name__ == '__main__':
-    for hunter in hunters:
+    for hunter in hunters[:1]:
         assistant = HunterAssistant(hunter)
         assistant.deliver_card()
         time.sleep(random.random()*60)
